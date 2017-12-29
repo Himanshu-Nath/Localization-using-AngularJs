cd4130948692c44fed98c1cbd6f14c948b064db8
Himanshu Nath <hnath723@gmail.com>
2017-12-29 14:43:32 +0530

    Added few changes regarding ui

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var bodyParser = require('body-parser');
 var app=express();
-var port_no = process.env.PORT || 3010;
+var port_no = process.env.PORT || 3011;
 
 /*------------------Routing Started ------------------------*/
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
@@ -9,11 +9,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-app.use(express.static(__dirname + '/app'));
+// app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.get('/',function(req,res){
-	res.sendFile( __dirname + "/app/" + 'index.html');
+app.get('*',function(req,res){
+	// res.sendFile( __dirname + "/app/" + 'index.html');
+	res.sendFile( __dirname + "/dist/index.html");
 });
 
 app.listen(port_no, function(){
